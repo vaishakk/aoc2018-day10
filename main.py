ae739fe7c4c11ebdb424d64457622b5ab5cdad0b
Vaishak K <vaishakk@gmail.com>
2022-05-18 23:54:43 +0200

    Implement scrape input from file.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def scrape_input(input_file='sample-input'):
 	positions = []
 	velocities = []
-	# ToDo
+	with open(input_file, 'r') as file:
+		input = file.read()
+	lines  = input.split('\n')
+	for line in lines:
+		position = line.split('>')[0].split('<')[1].split(',')
+		for i, p in enumerate(position):
+			position[i] = int(p)
+		positions.append(position)
+		velocity = line.split('>')[1].split('<')[1].split(',')
+		for i, v in enumerate(velocity):
+			velocity[i] = int(v)
+		velocities.append(velocity)
 	return positions, velocities
 
 def offset_positions_to_zero(positions):
