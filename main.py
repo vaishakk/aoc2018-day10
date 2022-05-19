04946efbdbe92e3604a26b78074e99fc54827cd7
Vaishak K <vaishakk@gmail.com>
2022-05-19 09:37:01 +0200

    Final commit
    * Write the output as an image.
    * Use findings of trials to speed up movement.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from operator import itemgetter
+from PIL import Image
+import numpy as np
+
+INITIAL_POS = 10630
+NUM_MOVES = 20
 
 def scrape_input(input_file='sample-input'):
 	positions = []
@@ -30,6 +35,20 @@ def move_one_step(positions, velocities):
 		positions[i] = position[0] + velocities[i][0], position[1] + velocities[i][1]
 	return positions
 
+def move_many_steps(positions, velocities, steps):
+	for i, position in enumerate(positions):
+		positions[i] = position[0] + steps*velocities[i][0], position[1] + steps*velocities[i][1]
+	return positions
+
+def write_image(positions, max_x, max_y, step):
+	image = np.zeros([max_x+1, max_y+1])
+	for i, p in enumerate(positions):
+		image[positions[i][0], positions[i][1]] = 255
+	array = np.array(image.transpose(), dtype=np.uint8)
+	new_image = Image.fromarray(array)
+	new_image.save('pos_{}.png'.format(step))
+
+
 def display_positions(positions, max_x, max_y):
 	step_y = int(max_y/100)
 	step_x = int(max_x/100)
@@ -39,7 +58,6 @@ def display_positions(positions, max_x, max_y):
 	for y in range(0, 100):
 		cols = filter(lambda p: p[1] == y, sampled_positions)
 		msg_row = ['.']*100
-		#print(list(cols))
 		for c in cols:
 			if c[0] == 100:
 				msg_row[99] = '#'
@@ -48,13 +66,12 @@ def display_positions(positions, max_x, max_y):
 		print(''.join([str(m) for m in msg_row]))
 
 positions, velocities = scrape_input('input')
-
-while(True):
+max_x = max(positions, key=itemgetter(0))[0]
+max_y = max(positions, key=itemgetter(1))[1]
+positions = move_many_steps(positions, velocities, INITIAL_POS)
+for i in range(0, NUM_MOVES):
 	positions = offset_positions_to_zero(positions)
 	max_x = max(positions, key=itemgetter(0))[0]
 	max_y = max(positions, key=itemgetter(1))[1]
-	display_positions(positions, max_x, max_y)
+	write_image(positions, max_x, max_y, i + INITIAL_POS)
 	positions = move_one_step(positions, velocities)
-	res = input('Press Q to quit and any other key to display next step.')
-	if res in ['q', 'Q']:
-		break;
